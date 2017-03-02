3b328d8a090eca015afb0edcac2c40d32868d1ef
Julien Flamant <flamantjf@gmail.com>
2017-03-02 23:37:33 +0100

    add setup py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='bispy',
+      version='1.0',
+      description="BiSPy",
+      long_description="",
+      author='TODO',
+      author_email='todo@example.org',
+      license='TODO',
+      packages=['bispy'],
+      zip_safe=False,
+      install_requires=[
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
