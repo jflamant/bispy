72ca933dfa563c6b9d1a0c21e0b1b4da7355c06c
Julien Flamant <flamantjf@gmail.com>
2017-07-18 12:30:13 +0200

    add: spectrum dependency

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'matplotlib', 'numpy-quaternion'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'numpy-quaternion', 'spectrum'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
