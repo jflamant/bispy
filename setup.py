2a26ad4c437754d9f9887789319f597b694c4a05
Julien Flamant <flamantjf@gmail.com>
2024-08-01 13:11:03 +0200

    update PyPi version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.2.dev',
+    version='0.9.3.dev',
 
     description='An open-source python framework for processing \
     bivariate signals.',
